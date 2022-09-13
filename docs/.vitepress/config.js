e6a84ecc01bc491fc831bc97649dbd3ed4d5251e
Karl <10119002+koujialong@user.noreply.gitee.com>
2022-09-13 19:37:08 +0800

    feat: ts class

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -49,7 +49,8 @@ function sideBarFe() {
       text: 'ts',
       collapsible: true,
       items: [
-        {text: 'interface 接口', link: '/fe/ts/interface.md'}
+        {text: 'interface 接口', link: '/fe/ts/interface.md'},
+        {text: 'class 类', link: '/fe/ts/class.md'}
       ]
     },
     {
